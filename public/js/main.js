ff6bb2a44e9fe5ddf9224df317c7e19969a0c09e
AbhijeetR9 <79084621+AbhijeetR9@users.noreply.github.com>
2021-07-10 11:57:12 +0530

    weather app hosting

---
+++ public/js/main.js
@@ -0,0 +1,118 @@
+
+
+
+const cityName = document.getElementById("cityName");
+const submitBtn = document.getElementById("submitBtn");
+const city_Name = document.getElementById("city_Name");
+const tempInCel = document.getElementById("tempInCel");
+const temp_Status = document.getElementById("temp_Status");
+
+
+const data_hide = document.getElementById("data_hide")
+
+// To display search button whenever click on city Name
+
+submitBtn.style.display = 'none';
+cityName.addEventListener('click',()=>{
+
+    
+submitBtn.style.display = 'block';
+
+})
+
+
+const getInfo = async (e) => {
+    // alert("Hey ..")
+
+    e.preventDefault();
+
+
+    let cityVal = cityName.value;
+    if (cityVal === "") {
+
+        city_Name.innerText = `Please Write the City Name before You Search.`
+
+     
+        
+
+    }
+
+    else if (/^\s+$/.test(cityVal)) {
+        // console.log("Contains Spacing");
+        city_Name.innerText = `Please Enter City Name Properly :)`
+        
+    }
+  
+
+    else {
+
+        try {
+
+            data_hide.style.visibility= "visible";
+
+            
+            data_hide.style.visi
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=ffe5065224a4e55cd208c32ad7d6bae8`
+
+            const response = await fetch(url);
+
+
+
+
+            const data = await response.json()
+
+
+            const arrData = [data];
+            // console.log(arrData);
+
+
+            city_Name.innerText = `${arrData[0].name}, ${arrData[0].sys.country}`;
+            tempInCel.innerText = arrData[0].main.temp;
+            // temp_Status.innerText = arrData[0].weather[0].main;
+
+            const tempMood = arrData[0].weather[0].main;
+
+            // Condition to check sunny or cloudy
+            if (tempMood === "Clear") {
+                temp_Status.innerHTML = `<i class="fas fa-sun" style="color: #eccc68;" ></i>`
+                
+            }
+
+            else if (tempMood === "Clouds") {
+
+                temp_Status.innerHTML = `<i class="fas fa-cloud" style="color: #f1f2f6;" ></i>`
+            }
+
+            else if (tempMood === "Rain") {
+                temp_Status.innerHTML = `<i class="fas fa-cloud-rain" style="color: #a4b0be;" ></i>`
+                
+            }
+
+            else {
+
+                temp_Status.innerHTML = `<i class="fas fa-sun" style="color: #eccc68;" ></i>`
+                
+            }
+
+           
+
+
+           
+        }
+
+
+        catch {
+            city_Name.innerText = `Please Enter Your City Name Properly.`
+            
+        }
+    }
+
+
+}
+
+
+submitBtn.addEventListener('click', getInfo)
+
+
+
+
---
+++ src/app.js
@@ -0,0 +1,78 @@
+
+
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const hbs = require('hbs');
+
+
+
+// Server Port or Hostname
+const port = process.env.PORT || 80;
+const hostname = '127.0.0.1'
+
+
+
+
+
+// To  set the templates engines
+app.set('view engine', 'hbs');
+
+
+const templates_path = path.join(__dirname,'../templates/views');
+const partials_Path = path.join(__dirname,'../templates/partials');
+
+app.set('views', templates_path)
+hbs.registerPartials(partials_Path)
+
+
+// public Static path
+
+// console.log(path.join(__dirname,'../public'));
+
+const staticPath = path.join(__dirname,'../public');
+
+app.use(express.static(staticPath))
+
+
+
+// Routing or Navigating the Page.
+app.get('/', (req, res)=>{
+
+    // res.send("Hello to Home Page....:)")
+    res.render('index')
+
+
+})
+
+app.get('/about', (req, res)=>{
+
+    
+    // res.send("Welcome to About Page")
+    res.render('about')
+
+})
+
+
+app.get('/weather', (req, res)=>{
+
+    // res.send("It's a Weather Page. ....._.:)(:")
+    res.render('weather')
+
+})
+
+app.get('*', (req, res)=>{
+
+    // res.send("Oops!!  404 - Page Not Found...")
+    res.render('404error',{
+        errmsg : "Oops!!! Page Not Found."
+    })
+
+
+})
+
+
+app.listen(port, hostname, ()=>{
+    console.log(`Server is Starting at (press Ctrl and click to Visit) ->  http://${hostname} . `);
+} )
